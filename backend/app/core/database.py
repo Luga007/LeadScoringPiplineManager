3dca66c08cc05decb776639cf6ed95173940c88d
Luga007 <yunusovaugust@gmail.com>
2026-04-27 23:11:22 +0500

    fixed

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -7,9 +7,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # -----------------------
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# -----------------------
-# SAFETY CHECK (IMPORTANT)
-# -----------------------
+
 if not DATABASE_URL:
     raise RuntimeError(
         "DATABASE_URL is not set. "
